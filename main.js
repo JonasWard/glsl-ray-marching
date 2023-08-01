5c6078b7cec08a24f3aa2c17a21218c2ab756017
JonasWard <43600893+JonasWard@users.noreply.github.com>
2023-08-01 10:41:17 +0200

    cleaning saving code

--- main.js
+++ main.js
@@ -28,21 +28,23 @@ const data = {
   'all the data in this file': 0,
 };
 
-const saving = new (function () {
-  this.saving = () => {
-    var a = document.createElement('a');
-    document.body.appendChild(a);
-    a.style = 'display: none';
-    const fileName = 'pattern_settings.json';
-    var json = JSON.stringify(data),
-      blob = new Blob([json], { type: 'octet/stream' }),
-      url = window.URL.createObjectURL(blob);
-    a.href = url;
-    a.download = fileName;
-    a.click();
-    window.URL.revokeObjectURL(url);
-  };
-})();
+const saveMethod = () => {
+  const a = document.createElement('a');
+  document.body.appendChild(a);
+  a.style = 'display: none';
+  const fileName = 'pattern_settings.json';
+  const jsonObject = JSON.stringify(data),
+    blob = new Blob([jsonObject], { type: 'octet/stream' }),
+    url = window.URL.createObjectURL(blob);
+  a.href = url;
+  a.download = fileName;
+  a.click();
+  window.URL.revokeObjectURL(url);
+};
+
+const saving = {
+  saving: saveMethod,
+};
 
 const scales = {
   preProcessingA: 2,
