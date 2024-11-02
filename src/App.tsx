d4e87e6623254b16a918468576c7ee43d3e2420d
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-11-02 21:39:41 +0100

    feat: app with url-string state

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { parserObjects } from './modelDefinition/model';
 import { ParametricInput } from './Components/parametrics/ParametricInput';
-import { DataEntry, getStateValue } from 'url-safe-bitpacking';
+import { DataEntry } from 'url-safe-bitpacking';
 import { versionEnumSemantics } from './modelDefinition/types/semantics';
-import { InitWebGL } from './config/init';
 import { useData } from './state';
+import { ThreeCanvas } from './webgl/ThreeCanvas';
+import { useParams } from 'react-router-dom';
+
+const defaultState = undefined;
 
 export const App: React.FC = () => {
+  const { stateString } = useParams();
   const data = useData((s) => s.data);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    useData.getState().setData(parserObjects.parser(stateString ?? defaultState));
+  }, []);
+
+  useEffect(() => {
+    window.history.replaceState(null, 'Same Page Title', `/glslRayMarching/#${parserObjects.stringify(data)}`);
+  }, [data]);
 
   const updateEntry = (update: DataEntry | DataEntry[]): void => useData.getState().setData(parserObjects.updater(useData.getState().data, update));
 
-  const updateWebGLContext = () => canvasRef.current && InitWebGL(canvasRef.current, getStateValue(useData.getState().data));
-
-  useEffect(() => {
-    updateWebGLContext();
-  }, [data]);
-
-  useEffect(() => {
-    const onResize = () => updateWebGLContext();
-    window.addEventListener('resize', onResize);
-    return () => window.removeEventListener('resize', onResize);
-  }, []);
-
   return (
     <div>
-      <canvas id='webgl-canvas' ref={canvasRef} style={{ opacity: 0.5, backgroundColor: 'red' }}>
-        Your browser does not support HTML5
-      </canvas>
+      <ThreeCanvas />
       <ParametricInput data={data} updateEntry={updateEntry} versionEnumSemantics={versionEnumSemantics} />
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -5,4 +5,13 @@ import './index.css';
 import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route element={<App />} path='/:stateString' />
+        <Route element={<App />} path='/' />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
