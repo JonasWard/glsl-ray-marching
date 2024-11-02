347a0610aeab588fbc58dd2a08359d6cf2fc467b
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-11-02 21:38:33 +0100

    fix EnumDataRenderer

--- src/Components/parametrics/dataentryrenderers/EnumDataEntryRenderer.tsx
+++ src/Components/parametrics/dataentryrenderers/EnumDataEntryRenderer.tsx
@@ -1,21 +1,19 @@
 import { Select } from 'antd';
 import React from 'react';
 import { IconRenderer } from '../IconRenderer';
-import { VersionEnumSemantics } from 'url-safe-bitpacking';
+import { EnumSemantics } from 'url-safe-bitpacking';
 import { EnumDataEntry } from 'url-safe-bitpacking/dist/types';
-import { versionEnumSemantics } from '../../../modelDefinition/types/semantics';
 
 export interface IEnumDataEntryRendererProps {
   enumValue: EnumDataEntry;
   onChange: (newValue: EnumDataEntry) => void;
-  versionEnumSemantics?: VersionEnumSemantics;
+  versionEnumSemantics?: EnumSemantics;
 }
 
 const defaultSelectorValues = (max: number, key: string) => [...Array(max).keys()].map((i) => ({ value: i, label: `${key} ${i}` }));
-const getVersionEnumSemanticsMap = (enumValue: EnumDataEntry, versionEnumSemantics?: VersionEnumSemantics) => {
+const getVersionEnumSemanticsMap = (enumValue: EnumDataEntry, versionEnumSemantics?: EnumSemantics) => {
   if (versionEnumSemantics) {
     const filterEnumSemantics = Object.keys(versionEnumSemantics).filter((semantic) => enumValue.name.startsWith(semantic));
-    console.log(enumValue, filterEnumSemantics);
     if (filterEnumSemantics.length) return versionEnumSemantics[filterEnumSemantics[0]];
   }
 
