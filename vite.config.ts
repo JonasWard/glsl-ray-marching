bb04ab09af82ca48c35af9cfa16f551568f8c55e
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-11-03 13:23:45 +0100

    chore: more making it build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ dns.setDefaultResultOrder('verbatim');
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3017,
+    port: 3000,
   },
   build: {
     outDir: './build',
