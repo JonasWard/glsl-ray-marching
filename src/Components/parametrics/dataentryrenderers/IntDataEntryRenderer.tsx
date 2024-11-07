31186bc19957b58939bd0e9331b84960a1dd7395
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-11-07 10:17:55 +0100

    feat: 3 different displayStyles for intDataEntries

--- src/Components/parametrics/dataentryrenderers/IntDataEntryRenderer.tsx
+++ src/Components/parametrics/dataentryrenderers/IntDataEntryRenderer.tsx
@@ -1,4 +1,4 @@
-import { InputNumber } from 'antd';
+import { InputNumber, Select } from 'antd';
 import { IconRenderer } from '../IconRenderer';
 import React from 'react';
 import { IntDataEntry } from 'url-safe-bitpacking/dist/types';
@@ -9,17 +9,41 @@ export interface IIntDataEntryRendererProps {
   onChange: (newValue: IntDataEntry) => void;
   customMin?: number;
   customMax?: number;
-  asSlider?: boolean;
+  displayStyle?: 'slider' | 'dropdown' | 'input';
 }
 
-export const IntDataEntryRenderer: React.FC<IIntDataEntryRendererProps> = ({ int, onChange, customMax, customMin, asSlider }) => (
-  <SliderWrapper
-    icon={<IconRenderer name={int.name} type={int.type} size={20} />}
-    step={1}
-    value={int.value}
-    onChange={(value: number) => onChange({ ...int, value })}
-    min={customMin ?? int.min}
-    max={customMax ?? int.max}
-    precision={0}
-  />
-);
+export const IntDataEntryRenderer: React.FC<IIntDataEntryRendererProps> = ({ int, onChange, customMax, customMin, displayStyle = 'slider' }) => {
+  switch (displayStyle) {
+    case 'slider':
+      return (
+        <SliderWrapper
+          icon={<IconRenderer name={int.name} type={int.type} size={20} />}
+          step={1}
+          value={int.value}
+          onChange={(value: number) => onChange({ ...int, value })}
+          min={customMin ?? int.min}
+          max={customMax ?? int.max}
+          precision={0}
+        />
+      );
+    case 'input':
+      return (
+        <InputNumber
+          step={1}
+          value={int.value}
+          onChange={(value: number | null) => value !== null && onChange({ ...int, value })}
+          min={customMin ?? int.min}
+          max={customMax ?? int.max}
+          precision={0}
+        />
+      );
+    case 'dropdown':
+      return (
+        <Select
+          value={int.value}
+          options={[...Array(int.max - int.min + 1).keys()].map((v) => ({ value: v + int.min, label: v + int.min }))}
+          onChange={(value: number) => onChange({ ...int, value })}
+        ></Select>
+      );
+  }
+};
