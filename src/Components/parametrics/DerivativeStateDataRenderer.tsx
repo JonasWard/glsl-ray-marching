d9d074c037b1b98e93b32ad08eef407687c7fc2a
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-11-07 10:02:29 +0100

    fix: derivative state rendering corrected

--- src/Components/parametrics/DerivativeStateDataRenderer.tsx
+++ src/Components/parametrics/DerivativeStateDataRenderer.tsx
@@ -1,9 +1,11 @@
-import { DataEntry } from 'url-safe-bitpacking';
+import { DataEntry, IntData } from 'url-safe-bitpacking';
 import { EnumSemantics, StateDataType } from 'url-safe-bitpacking';
 import { DisplayType, getDisplayType, StateDataRenderer } from './StateDataRenderer';
 import { ViewWrapper } from './ViewWrapper';
 import React from 'react';
 import { DataEntryRenderer } from './dataentryrenderers/DataEntryRenderer';
+import { ArrayDerivativeStateDataRenderer } from './ArrayDerivativeStateDataRenderer';
+import { IntDataEntry } from 'url-safe-bitpacking/dist/types';
 
 type IDerivativeStateDataRenderer = {
   s: DataEntry;
@@ -33,20 +35,32 @@ export const DerivativeStateDataRenderer: React.FC<IDerivativeStateDataRenderer>
 }) => (
   <ViewWrapper
     key={name}
-    displayType={getDisplayType(s.internalName!, displayTypeMap)}
-    name={s.internalName!}
+    displayType={getDisplayType(s.name!, displayTypeMap)}
+    name={s.name!}
     activeName={activeName}
     setActiveName={setActiveName}
     disabled={disabled}
   >
-    <DataEntryRenderer asSlider={asSlider} key={name} dataEntry={s} updateEntry={updateEntry} versionEnumSemantics={versionEnumSemantics} />
     {Array.isArray(v) ? (
-      v.map((locV) => (
+      <ArrayDerivativeStateDataRenderer
+        s={s as IntDataEntry}
+        v={v}
+        versionEnumSemantics={versionEnumSemantics}
+        updateEntry={updateEntry}
+        displayTypeMap={displayTypeMap}
+        activeName={activeName}
+        setActiveName={setActiveName}
+        disabled={disabled}
+        asSlider={asSlider}
+      />
+    ) : (
+      <>
+        <DataEntryRenderer asSlider={asSlider} key={name} dataEntry={s} updateEntry={updateEntry} versionEnumSemantics={versionEnumSemantics} />
         <StateDataRenderer
           asSlider={asSlider}
           key={`${name}-subdata`}
-          name={name}
-          data={locV}
+          name={''}
+          data={v}
           versionEnumSemantics={versionEnumSemantics}
           updateEntry={updateEntry}
           displayType={getDisplayType(name, displayTypeMap)}
@@ -55,21 +69,7 @@ export const DerivativeStateDataRenderer: React.FC<IDerivativeStateDataRenderer>
           setActiveName={setActiveName}
           disabled={disabled}
         />
-      ))
-    ) : (
-      <StateDataRenderer
-        asSlider={asSlider}
-        key={`${name}-subdata`}
-        name={name}
-        data={v}
-        versionEnumSemantics={versionEnumSemantics}
-        updateEntry={updateEntry}
-        displayType={getDisplayType(name, displayTypeMap)}
-        displayTypeMap={displayTypeMap}
-        activeName={activeName}
-        setActiveName={setActiveName}
-        disabled={disabled}
-      />
+      </>
     )}
   </ViewWrapper>
 );
